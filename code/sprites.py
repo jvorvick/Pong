f3a793da39f931167dd5560eb315f6867bf69c66
jvorvick <49791235+jvorvick@users.noreply.github.com>
2025-09-01 13:17:42 -0700

    Pong -- exercise solution init

---
+++ code/sprites.py
@@ -0,0 +1,46 @@
+from settings import *
+from random import choice, uniform
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+
+        # image
+        self.image = pygame.Surface(SIZE['paddle'])
+        self.image.fill(COLORS['paddle'])
+
+        # rect & movement
+        self.rect = self.image.get_frect(center = POS['player'])
+        self.direction = 0
+        self.speed = SPEED['player']
+
+    def move(self, dt):
+        self.rect.centery += self.direction * self.speed * dt
+        self.rect.top = 0 if self.rect.top < 0 else self.rect.top
+        self.rect.bottom = WINDOW_HEIGHT if self.rect.bottom > WINDOW_HEIGHT else self.rect.bottom
+
+    def get_direction(self):
+        keys = pygame.key.get_pressed()
+        self.direction = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+    def update(self, dt):
+        self.get_direction()
+        self.move(dt)
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, groups, paddle_sprites):
+        super().__init__(groups)
+
+        #image
+        self.image = pygame.Surface(SIZE['ball'])
+        self.image.fill(COLORS['ball'])
+
+        # rect & movement
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.Vector2(choice((1,-1)),uniform(0.7, 0.8) * choice((-1,1)))
+
+    def move(self, dt):
+        self.rect.center += self.direction * SPEED['ball'] * dt
+
+    def update(self, dt):
+        self.move(dt)
