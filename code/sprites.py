b05a4ee7b04d8fee5129604bc266b19cacd66c71
jvorvick <49791235+jvorvick@users.noreply.github.com>
2025-09-02 11:02:17 -0700

    Pong first part finish

--- code/sprites.py
+++ code/sprites.py
@@ -6,8 +6,8 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
 
         # image
-        self.image = pygame.Surface(SIZE['paddle'])
-        self.image.fill(COLORS['paddle'])
+        self.image = pygame.Surface(SIZE['paddle'], pygame.SRCALPHA)
+        pygame.draw.rect(self.image, COLORS['paddle'], pygame.FRect((0,0), SIZE['paddle']), 0, 4)
 
         # rect & movement
         self.rect = self.image.get_frect(center = POS['player'])
@@ -32,8 +32,8 @@ class Ball(pygame.sprite.Sprite):
         super().__init__(groups)
 
         #image
-        self.image = pygame.Surface(SIZE['ball'])
-        self.image.fill(COLORS['ball'])
+        self.image = pygame.Surface(SIZE['ball'], pygame.SRCALPHA)
+        pygame.draw.circle(self.image, COLORS['ball'], (SIZE['ball'][0] / 2,SIZE['ball'][1] / 2), SIZE['ball'][0] / 2)
 
         # rect & movement
         self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
@@ -42,5 +42,23 @@ class Ball(pygame.sprite.Sprite):
     def move(self, dt):
         self.rect.center += self.direction * SPEED['ball'] * dt
 
+    def wall_collision(self):
+        if self.rect.top <= 0:
+            self.rect.top = 0
+            self.direction.y *= -1
+
+        if self.rect.bottom >= WINDOW_HEIGHT:
+            self.rect.bottom = WINDOW_HEIGHT
+            self.direction.y *= -1
+
+        if self.rect.right >= WINDOW_WIDTH:
+            self.rect.right = WINDOW_WIDTH
+            self.direction.x *= -1
+
+        if self.rect.left <= 0:
+            self.rect.left = 0
+            self.direction.x *= -1
+
     def update(self, dt):
         self.move(dt)
+        self.wall_collision()
